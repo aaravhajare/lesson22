43f5f30f5c57d360b0217d5248f307ea0f1d8e46
aaravhajare <hajareaarav66@gmail.com>
2025-04-13 09:44:41 +0530

    Arrays

---
+++ Arrays.py
@@ -0,0 +1,13 @@
+import array as a
+
+#create an array 
+b = a.array("i" , [1,2,3,4,5])
+print(b)
+
+# number of repetation 
+c = a.array("i", [1,2,3,1,2,3,4,2])
+print(b.count(3))
+
+# reverse the array
+d = [1,2,3,4,4]
+print(d.reverse())
