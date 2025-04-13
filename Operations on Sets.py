0dc83759a343a45d04a6ff7071746e77b24ea5ea
aaravhajare <hajareaarav66@gmail.com>
2025-04-13 09:34:52 +0530

    Operations on Sets

---
+++ Operations on Sets.py
@@ -0,0 +1,19 @@
+my_set = {1,2,3}
+print(my_set)
+
+# multiple data types
+set1 = {1 , 2.3 , "hello" , True , (1,2,4) }
+print(set1)
+
+# not have duplicates 
+seti = {1,2,3,4,3,2}
+print(seti)
+
+# set from list
+set_1 = set([1,2,3,4,5,2,3])
+print(set_1)
+
+# remove 1st element 
+set2 = {1,2,3,4,5}
+set2.remove(1)
+print(set2)
