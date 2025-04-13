57bcf2d9bacce63ceae6d857faba718ad567d939
aaravhajare <hajareaarav66@gmail.com>
2025-04-13 09:46:56 +0530

    Arrays

--- Arrays.py
+++ Arrays.py
@@ -6,8 +6,8 @@ print(b)
 
 # number of repetation 
 c = a.array("i", [1,2,3,1,2,3,4,2])
-print(b.count(3))
+print(c.count(3))
 
 # reverse the array
-d = [1,2,3,4,4]
-print(d.reverse())
+d = a.array("i" , [1,2,3,4]) 
+print(d[::-1])
